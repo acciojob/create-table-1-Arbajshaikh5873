ba145770635a7e2b9cbcc85c4a57cc9f1f872227
Arbaj Jamir Shaikh <155817228+Arbajshaikh5873@users.noreply.github.com>
2025-07-31 09:56:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function insert_Row() {
     //Write your code here
-  
+  let ele_tbl = document.getElementById("sampleTable");
+
+	let contain = ele_tbl.innerHtml;
+
+	let result = "<tr><td>New Cell1</td><td>New Cell2</td></tr>";
+
+	ele_tbl.innerHtml = result + contain;
+	
   
 }
