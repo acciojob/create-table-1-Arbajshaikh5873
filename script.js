ef6ee016687ac6fb9aaf427ac98e545a119030a9
Arbaj Jamir Shaikh <155817228+Arbajshaikh5873@users.noreply.github.com>
2025-07-31 10:07:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function insert_Row() {
 	let row1 = ele_tbl.insertRow(0);
 
 	let col1 = row1.insertCell(0);
-	let col1 = row1.insertCell(1);
+	let col2 = row1.insertCell(1);
 
 	col1.textContent = "New Cell1";
 	col2.textContent = "New Cell2";
